c5934191b81f3a6588e01c684cceecc51704f41d
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-15 15:48:42 +0300

    fix: cleaning

--- temp/src/utils/randomInt.js
+++
@@ -1,5 +0,0 @@
-const randomInt = (min, max) => {
-	return Math.floor(Math.random() * (max - min) + min)
-};
-
-export  default randomInt;
--- temp/src/utils/setDependencyName.js
+++
@@ -1,8 +0,0 @@
-const setDependencyName = (a, idToSearch) => {
-	a = a.filter(item => {
-		return item.id === idToSearch
-	})
-	return a[0]
-}
-
-export default setDependencyName;
