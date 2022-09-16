fa266aaceafc75c86f49c7ba88c227cfd0814c16
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-16 11:54:29 +0300

    heroku

--- start-server.sh
+++ start-server.sh
@@ -3,7 +3,7 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd server; python manage.py createsuperuser --no-input)
 fi
-cd server
+
 gunicorn rentserv.wsgi --bind 0.0.0.0:8020 --workers 3
 # &
 # nginx -g "daemon off;"
