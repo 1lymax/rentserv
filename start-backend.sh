18d468a814b81f01c21faaa0740e85af123a4c26
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-20 19:21:50 +0300

    fixing cors

--- start-backend.sh
+++ start-backend.sh
@@ -3,7 +3,7 @@
 
  if [ -n "$PORT" ] ; then
  (gunicorn rentserv.wsgi --bind 0.0.0.0:$PORT --workers 3) ; else
- (gunicorn rentserv.wsgi --bind 0.0.0.0:7000 --workers 3)
+ (gunicorn rentserv.wsgi --bind 0.0.0.0:8000 --workers 3)
  fi
 
 #gunicorn rentserv.wsgi --bind 0.0.0.0:7000 --workers 3
