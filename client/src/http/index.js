8d558c9f0c2fe05a6103fe72382d667aff76599c
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-20 11:36:24 +0300

    splitting backend and frontend. frontend

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+import {REACT_APP_API_CLOUD_URL} from "../utils/consts";
 
+const API_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : REACT_APP_API_CLOUD_URL
 
 const $host = axios.create({
-	baseURL: process.env.REACT_APP_API_URL
+	baseURL: API_URL
 })
 
 const $authHost = axios.create({
-	baseURL: process.env.REACT_APP_API_URL
+	baseURL: API_URL
 })
 
 const authInterceptor = config => {
