5ff9aa39cb024d362668f349f6698e972ba88699
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-20 16:25:55 +0300

    splitting backend and frontend.

--- start-backend.sh
+++ start-backend.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # start.sh
 
-if [ -n "$PORT" ] ; then
-(gunicorn rentserv.wsgi --bind 0.0.0.0:$PORT --workers 3 --daemon) ; else
-(gunicorn rentserv.wsgi --bind 0.0.0.0:8020 --workers 3 --daemon)
-fi
+# if [ -n "$PORT" ] ; then
+# (gunicorn rentserv.wsgi --bind 0.0.0.0:$PORT --workers 3 --daemon) ; else
+# (gunicorn rentserv.wsgi --bind 0.0.0.0:8020 --workers 3 --daemon)
+# fi
+
+gunicorn rentserv.wsgi --bind 0.0.0.0:$PORT --workers 3 --daemon
