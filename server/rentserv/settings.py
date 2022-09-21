c63cf58201b80622bc8e9f47ecea8d56cf1e20e2
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-21 18:55:58 +0300

    g_k8s db test

--- server/rentserv/settings.py
+++ server/rentserv/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'rentserv.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rentserv',
-        'USER': 'rentserv',
-        'PASSWORD': '123',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'rentserv',
+#         'USER': 'rentserv',
+#         'PASSWORD': '123',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
