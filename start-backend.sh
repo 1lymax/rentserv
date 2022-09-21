0158c137c2fda2792d4b552a91f7852c04c051c9
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-21 14:36:30 +0300

    g_k8s

--- start-backend.sh
+++ start-backend.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # start_backend.sh
 
+python manage.py makemigrations
+python manage.py migrate
  if [ -n "$PORT" ] ; then
  (gunicorn rentserv.wsgi --bind 0.0.0.0:$PORT --workers 3) ; else
  (gunicorn rentserv.wsgi --bind 0.0.0.0:8000 --workers 3)
