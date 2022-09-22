1f0c0efbf4067b453420f12c18b5311b62bacf5c
1lymax <59887205+1lymax@users.noreply.github.com>
2022-09-22 12:15:54 +0300

    heroku

--- server/rentserv/settings.py
+++ server/rentserv/settings.py
@@ -92,16 +92,13 @@ WSGI_APPLICATION = 'rentserv.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'rentserv',
-#         'USER': 'rentserv',
-#         'PASSWORD': '123',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+
+    }
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
